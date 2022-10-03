9c7b470886954d8c23751e7c68774743eb8acf82
Omer Karakus <omer.karakus@lonres.com>
2022-10-03 18:08:53 +0100

    add type declaration

--- src/components/atoms/Editor/index.tsx
+++ src/components/atoms/Editor/index.tsx
@@ -2,14 +2,29 @@ import React, { useState } from "react";
 import { createEditor } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
 
-const initialValue = [
+// TypeScript users only add this code
+import { BaseEditor, Descendant } from "slate";
+import { ReactEditor } from "slate-react";
+
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor;
+    Element: CustomElement;
+    Text: CustomText;
+  }
+}
+
+const initialValue: Descendant[] = [
   {
     type: "paragraph",
     children: [{ text: "A line of text in a paragraph." }],
   },
 ];
 
-const Editor = () => {
+const App = () => {
   const [editor] = useState(() => withReact(createEditor()));
 
   return (
@@ -19,4 +34,4 @@ const Editor = () => {
   );
 };
 
-export default Editor;
+export default App;
